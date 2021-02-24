3ab40882df9101da7e7658cc30dbbdaeb7751db3
Thiago <a00279945@student.ait.ie>
2021-02-24 12:48:31 +0000

    second day of nlw

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express, { response } from 'express';
+import 'reflect-metadata';
+import express from "express";
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get("/users",(request,response) => {
-    return response.json({message: "Hello World!"})
-});
-
-app.post("/", (request, response) => {
-    return response.json({message: "os dados foram salvos com sucesso"})
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, ()=> console.log("The server is running"));
