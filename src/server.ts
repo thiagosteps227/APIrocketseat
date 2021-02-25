f2e3eb13e5e29b23a52421453f9122b5a0a7bf9f
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-25 11:16:48 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ import { router } from './routes';
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use(router)
 
 app.listen(3333, ()=> console.log("The server is running"));
